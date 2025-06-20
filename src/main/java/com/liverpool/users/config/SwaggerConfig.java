8c5c7ba893970e71951216c5aa70776f50c7143f
fenixorion <fenixorionm@gmaail.com>
2025-06-19 22:26:20 -0600

    Changing name API

--- src/main/java/com/liverpool/users/config/SwaggerConfig.java
+++ src/main/java/com/liverpool/users/config/SwaggerConfig.java
@@ -5,5 +5,5 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "User API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Liverpool API", version = "v1"))
 public class SwaggerConfig {}
